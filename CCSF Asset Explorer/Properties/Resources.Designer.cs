67072adec2db01cbe69092fff11df407c353de50
Miguel Queiroz <miguelchocolate@live.com>
2022-12-04 13:15:43 -0300

    *Remove old resources

--- CCSF Asset Explorer/Properties/Resources.Designer.cs
+++ CCSF Asset Explorer/Properties/Resources.Designer.cs
@@ -93,16 +93,6 @@ namespace CCSF_Asset_Explorer.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Capa {
-            get {
-                object obj = ResourceManager.GetObject("Capa", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap Capa1 {
             get {
                 object obj = ResourceManager.GetObject("Capa1", resourceCulture);
